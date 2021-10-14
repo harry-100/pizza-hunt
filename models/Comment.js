b2cce400db356e1c13de0eded8e6e156481036d4
Harry Shah <harvinder.shah@gmail.com>
2021-10-14 22:05:29 +0530

    comments routes added

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+const { Schema, Model } = require('mongoose');
+
+const CommentSchema = new Schema({
+    writtenBy: {
+        type: String
+    },
+    commentBody: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;
