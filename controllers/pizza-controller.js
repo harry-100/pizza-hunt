4c8f5ea6985d7d9d9f35704757e657b5c657dd47
Harry Shah <harvinder.shah@gmail.com>
2021-10-14 13:23:26 +0530

    routes added

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,65 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+    // get all pizzas
+    getAllPizza(req, res) {
+        Pizza.find({})
+        .then(dbPizzaData => res.join(dbPizzaData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    // get pizza by id
+    getPizzaById({ params }, res) {
+        Pizza.findOne({_id: params.id})
+        .then(dbPizzaData => {
+            // if no pizza is found, send 404 error
+            if(!dbPizzaData){
+                res.status(404).json({ message: 'No pizza found with this id'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    // create Pizza
+    createPizza({ body }, res) {
+        Pizza.create(body)
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.status(400).json(err));
+    },
+    // update a Pizza by id
+    updatePizza({ params, body }, res) {
+        // if we don't set new:true, it will Mongoose will return old document. By setting it to new, we are asking it return new updated document
+        Pizza.findOneAndUpdate({ _id: params.id }, body, {new: true })
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    //  delete a Pizza
+    deletePizza({ params }, res){
+        Pizza.findOneAndDelete({ _id: params.id})
+        .then(dbPizzaData => {
+            if(!dbPizzaData){
+                res.status(404).json({ message: 'No pizza found with this id'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+};
+
+
+module.exports = pizzaController;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
-
+const apiRoutes = require('./api');
+//  add prefix of './api'  to all of the api routes imported from from  the api directory
 router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
